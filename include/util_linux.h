6992adc7372d5707ac35bb0db8ad555805b3afd3
MG# <matthew.gream@pobox.com>
2025-04-25 21:32:20 +0200

    refactor

--- include/util_linux.h
+++ include/util_linux.h
@@ -2,8 +2,15 @@
 // -----------------------------------------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------------------------
 
-unsigned short __unpack_h(const unsigned char *x) { return (x[0] << 8) | x[1]; }
-float __unpack_f(const unsigned char *x) {
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <stdint.h>
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+uint32_t __unpack_h(const uint8_t *x) { return (x[0] << 8) | x[1]; }
+float __unpack_f(const uint8_t *x) {
     union {
         uint32_t i;
         float f;
@@ -13,13 +20,18 @@ float __unpack_f(const unsigned char *x) {
     return u.f;
 }
 
-bool intervalable(const time_t period, time_t *previous) {
-    const time_t current = time(NULL);
-    if (current >= (*previous + period)) {
-        *previous = current;
-        return true;
+time_t intervalable(const time_t interval, time_t *last) {
+    time_t now = time(NULL);
+    if (*last == 0) {
+        *last = now;
+        return 0;
     }
-    return false;
+    if ((now - *last) > interval) {
+        const time_t diff = now - *last;
+        *last = now;
+        return diff;
+    }
+    return 0;
 }
 
 void hexdump(const unsigned char *data, const int size, const char *prefix) {
@@ -45,6 +57,23 @@ void hexdump(const unsigned char *data, const int size, const char *prefix) {
         }
         printf("\n");
     }
+}
+
+bool is_reasonable_json(const unsigned char *packet, const int length) {
+    if (length < 2)
+        return false;
+    if (packet[0] != '{' || packet[length - 1] != '}')
+        return false;
+    for (int index = 0; index < length; index++)
+        if (!isprint(packet[index]))
+            return false;
+    return true;
+}
+
+#define EMA_ALPHA 0.2f
+void ema_update(unsigned char value, unsigned char *value_ema, unsigned long *value_cnt) {
+    *value_ema =
+        (*value_cnt)++ == 0 ? value : (unsigned char)((EMA_ALPHA * (float)value) + ((1.0f - EMA_ALPHA) * (*value_ema)));
 }
 
 // -----------------------------------------------------------------------------------------------------------------------------------------
